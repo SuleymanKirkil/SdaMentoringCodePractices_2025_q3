7eccf842bba70d6dfaf2fdd69ce9ad134d92fa6d
Süleyman Kırkıl <ali@gel.com>
2025-09-07 16:59:02 +0300

    homeWork added

---
+++ src/test/java/assignments/day03/C01fistAssignment.java
@@ -0,0 +1,28 @@
+package assignments.day03;
+
+import org.junit.jupiter.api.Test;
+
+public class C01fistAssignment {
+
+    // Navigate to: https://opensource-demo.orangehrmlive.com/
+    // TODO: Locate username using absolute XPath
+    String absoluteUsernameXPath = ""; // Fill absolute path
+
+    // TODO: Locate username using relative XPath
+    String relativeUsernameXPath = ""; // Fill relative path
+    // TODO: Locate password using both methods
+
+    // TODO: Locate login button using both methods
+
+    // TODO: Test both approaches work
+
+    @Test
+    void relativeXpathTest() {
+    }
+
+    @Test
+    void absoltuXpathTest() {
+
+    }
+}
+
---
+++ src/test/java/assignments/day03/C02SecondAssingment.java
@@ -0,0 +1,14 @@
+package assignments.day03;
+
+public class C02SecondAssingment {
+    String url = "https://testpages.herokuapp.com/styled/index.html";
+// TODO: Navigate to test pages
+// TODO: Click on "Locators - Find By Playground Test Page"
+// TODO: Print current URL
+// TODO: Navigate back
+// TODO: Print URL after going back
+// TODO: Click on "WebDriver Example Page"
+// TODO: Print current URL
+// TODO: Enter value 20 in number input
+// TODO: Verify 'two, zero' message appears
+}
