3be118d92b11992af717415d9c00bd17b16f5f0b
Süleyman Kırkıl <ali@gel.com>
2025-09-07 16:55:11 +0300

    xPathComparison added

---
+++ src/test/java/tests/C10XpathComparisonTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class C10XpathComparisonTest {
+
+    // Navigate to: https://opensource-demo.orangehrmlive.com/
+    // TODO: Locate username using absolute XPath
+    String absoluteUsernameXPath = ""; // Fill absolute path
+
+    // TODO: Locate username using relative XPath
+    String relativeUsernameXPath = ""; // Fill relative path
+    // TODO: Locate password using both methods
+
+    // TODO: Locate login button using both methods
+
+    // TODO: Test both approaches work
+
+    @Test
+    void relativeXpathTest() {
+    }
+
+    @Test
+    void absoltuXpathTest() {
+
+    }
+}
+
